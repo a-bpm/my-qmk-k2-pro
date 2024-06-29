5b13e7993f033a48c3735b2547f92b803ad545f3
Andrew B <andrew.bryan.dev@gmail.com>
2024-06-29 16:31:22 -0400

    update map

--- keyboards/keychron/k2_pro/ansi/rgb/keymaps/a-bpm/keymap.c
+++ keyboards/keychron/k2_pro/ansi/rgb/keymaps/a-bpm/keymap.c
@@ -25,10 +25,10 @@ enum layers{
 };
 
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_ALT, KC_U, KC_DEL);
-const key_override_t left_key_override = ko_make_basic(MOD_MASK_ALT, KC_H, KC_LEFT);
-const key_override_t down_key_override = ko_make_basic(MOD_MASK_ALT, KC_J, KC_DOWN);
-const key_override_t up_key_override   = ko_make_basic(MOD_MASK_ALT, KC_K, KC_UP);
-const key_override_t right_key_override = ko_make_basic(MOD_MASK_ALT, KC_L, KC_RIGHT);
+const key_override_t left_key_override   = ko_make_basic(MOD_MASK_ALT, KC_H, KC_LEFT);
+const key_override_t down_key_override   = ko_make_basic(MOD_MASK_ALT, KC_J, KC_DOWN);
+const key_override_t up_key_override     = ko_make_basic(MOD_MASK_ALT, KC_K, KC_UP);
+const key_override_t right_key_override  = ko_make_basic(MOD_MASK_ALT, KC_L, KC_RIGHT);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &delete_key_override,
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [WIN_BASE] = LAYOUT_ansi_84(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_HOME,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_END,
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_HOME,  KC_END,   RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_DEL,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_F24,
      KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_MEDIA_PLAY_PAUSE,
      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_ENTER,
      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
